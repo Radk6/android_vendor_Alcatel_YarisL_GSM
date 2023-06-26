3ebab4b904dce1e2f59f21e761a855b6a09052eb
Radplay <radoslaw.ryszka006@gmail.com>
2023-06-26 13:07:46 +0200

    Update and rename popc5-vendor.mk to YARIS35_GSM-vendor.mk

---
+++ YARIS35_GSM-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Alcatel/YARIS35_GSM/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Alcatel/YARIS35_GSM/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Alcatel/YARIS35_GSM/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Alcatel/YARIS35_GSM/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Alcatel/YARIS35_GSM/proprietary/xbin,system/xbin)
+
