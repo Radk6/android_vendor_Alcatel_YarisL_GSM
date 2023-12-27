27d06272a6451c3e930fa6c37fa755eec759c424
Rad <63958709+Radk6@users.noreply.github.com>
2023-12-27 22:22:53 +0100

    Update and rename YARIS35_GSM-vendor.mk to YarisL_GSM-vendor.mk

---
+++ YarisL_GSM-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Alcatel/YarisL_GSM/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Alcatel/YarisL_GSM/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Alcatel/YarisL_GSM/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Alcatel/YarisL_GSM/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Alcatel/YarisL_GSM/proprietary/xbin,system/xbin)
+
